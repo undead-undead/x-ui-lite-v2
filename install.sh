17e668f9f6f54f66a3acf44c8974b8ad2044fbbe
undead-undead <244278928+undead-undead@users.noreply.github.com>
2026-01-16 22:32:00 +0800

    fix: uncomment if statement to fix syntax error

--- install.sh
+++ install.sh
@@ -230,7 +230,7 @@ install_xray() {
     
     # Updated to match the single static binary name in release
     local xray_lite_file="xray-linux-amd64"
-    # if [[ "$support_xdp" == "true" ]]; then
+    if [[ "$support_xdp" == "true" ]]; then
     #     xray_lite_file="${xray_lite_file}-xdp"
         echo -e "${green}Detected High-Performance Kernel: ${kernel_version}${plain}"
         echo -e "${green}Installing XDP-Enhanced Version / 安装 XDP 增强版${plain}"
